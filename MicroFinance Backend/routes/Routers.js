1d7f9b7da23d1c79656fd5f8b34198318d44a6e1
Muhammad Arsalan Khan <arsalangg05@gmail.com>
2026-02-13 19:06:45 +0500

    Rename OTP import to lowercase 'otp'

--- MicroFinance Backend/routes/Routers.js
+++ MicroFinance Backend/routes/Routers.js
@@ -3,7 +3,7 @@ const router = express.Router()
 import {loancategories,deletedcategory,updatecategory,getloancategories,} from "../controllers/admin/loancategoriesController.js"
 import { dayStatusCon } from "../controllers/admin/DayStatus.js"
 import { loanApproved } from "../controllers/admin/loanApproved.js"
-import { login, signup, OTP } from "../controllers/loginSignupController.js"
+import { login, signup, otp } from "../controllers/loginSignupController.js"
 import { loanRequest,getLoandata} from "../controllers/loanRequestController.js"
 import upload from "../services/multer.js"
 import { authCheck, authCheckAdmin } from "../middleware/authCheck.js"
@@ -12,7 +12,7 @@ import {loanApllicationReject, loanApllicationApproved, loanApllicationComplete,
 //Auth
 router.post("/login", login)
 router.post("/signup", signup)
-router.patch("/otp/:id", OTP)
+router.patch("/otp/:id", otp)
 
 //user
 //Loan request
